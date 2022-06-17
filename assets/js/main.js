3a6164da540388459a6c12f8e0e92b61ca557b48
belifkutlu <elifkutlu41@gmail.com>
2022-06-17 13:20:37 +0300

    last fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,76 @@
 let products = [];
+
 fetch("../../product-list.json")
   .then((response) => response.json())
   .then((productData) => {
     const categories = productData.responses[0][0].params.userCategories;
     const recommendedProducts =
       productData.responses[0][0].params.recommendedProducts;
+    const productKeys = Object.keys(recommendedProducts);
+    const firstCategoryKey = productKeys[0];
+
     products = recommendedProducts;
-    const productKeys = Object.keys(products);
 
     categories.forEach((category) => {
-      const categoriesWrapper = document.getElementsByClassName("sidebar")[0];
-      categoriesWrapper.innerHTML += `<div class="sidebar-item" data-key="${category}" onclick="handleTabClick('${category}')"><span>${category}</span></div>`;
+      const categoriesWrapper = document.getElementsByClassName("tab-bar")[0];
+      categoriesWrapper.innerHTML += `
+        <div 
+          class="tab-bar-item" 
+          data-key="${category}" 
+          onclick="handleTabClick('${category}')">
+          <span>${category}</span>
+        </div>`;
     });
 
-    drawSlider(productKeys[0]);
+    drawSlider(firstCategoryKey);
+    selectTabBar(firstCategoryKey);
   });
+
+function handleTabClick(productKey) {
+  selectTabBar(productKey);
+  drawSlider(productKey);
+}
+
+function selectTabBar(productKey) {
+  const tabBars = document.querySelectorAll(".tab-bar-item");
+
+  tabBars.forEach(function (tabBar) {
+    const dataKey = tabBar.getAttribute("data-key");
+
+    if (dataKey == productKey) {
+      tabBar.classList.add("active");
+    } else {
+      tabBar.classList.remove("active");
+    }
+  });
+}
+
+function drawSlider(productKey) {
+  const productsWrapper = document.getElementsByClassName("splide__list");
+  let splideList = "";
+  products[productKey].forEach(({ name, image, priceText }) => {
+    splideList += `
+      <div class="splide__slide product-card">
+        <img
+          class="product-img"
+          src="${image}"
+          alt=""
+        />
+        <h3 class="product-title">
+          ${name}
+        </h3>
+        <div class="price"><span>${priceText}</span></div>
+        <div class="shipping">
+          <img src="./assets/images/shipping.svg" alt="" />
+          <span>Ücretsiz Kargo</span>
+        </div>
+        <button class="add-basket-btn" onclick="addToBasket('${name}')">Sepete Ekle</button>
+      </div>
+    `;
+  });
+  productsWrapper[0].innerHTML = splideList;
+  mountSlider();
+}
 
 function mountSlider() {
   new Splide(".splide", {
@@ -43,54 +99,10 @@ function mountSlider() {
   }).mount();
 }
 
-function handleTabClick(productKey) {
-  drawSlider(productKey);
-}
-
-function drawSlider(productKey) {
-  const productsWrapper = document.getElementsByClassName("splide__list");
-  let splideList = "";
-  products[productKey].forEach(({ name, image, priceText }) => {
-    splideList += `
-      <div class="splide__slide product-card">
-        <img
-          class="product-img"
-          src="${image}"
-          alt=""
-        />
-        <h3 class="product-title">
-          ${name}
-        </h3>
-        <div class="price"><span>${priceText}</span></div>
-        <div class="shipping">
-          <img src="./assets/images/shipping.svg" alt="" />
-          <span>Ücretsiz Kargo</span>
-        </div>
-        <button class="add-basket-btn" onclick="addToBasket('${name}')">Sepete Ekle</button>
-      </div>
-    `;
-  });
-  productsWrapper[0].innerHTML = splideList;
-  selectSidebar(productKey);
-  mountSlider();
-}
-
-function selectSidebar(productKey) {
-  const sidebars = document.querySelectorAll(".sidebar-item");
-
-  sidebars.forEach(function (sidebar) {
-    const dataKey = sidebar.getAttribute("data-key");
-
-    if (dataKey == productKey) {
-      sidebar.classList.add("active");
-    } else [sidebar.classList.remove("active")];
-  });
-}
-
 function addToBasket(name) {
   new Noty({
     type: "information",
-    layout: "topRight",
+    layout: "bottomRight",
     text: `${name} adlı ürün sepetine eklendi.`,
     theme: "relax",
     timeout: 2000,
